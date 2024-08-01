9a0401aa0bd55b3a7c0dea8198bbe42236adec54
somanathigeetha <somanathijabili837@gmail.com>
2024-08-01 11:41:37 +0530

    jabiliii

---
+++ domain.java
@@ -0,0 +1,39 @@
+class domain
+{
+public static void main(String args[])
+{
+char a='8';
+if(a>='0' && a>='9')
+{
+System.out.println("character");
+}
+else
+{
+System.out.println("number");
+switch(8)
+{
+case 1: System.out.println("I");
+        break;
+case 2: System.out.println("II");
+         break;
+case 3: System.out.println("III");
+         break;
+case 4: System.out.println("IV");
+        break;
+case 5: System.out.println("V");
+        break;
+case 6: System.out.println("VI");
+        break;
+case 7: System.out.println("VII");
+        break;
+case 8: System.out.println("VIII");
+        break;
+case 9: System.out.println("IX");
+        break;
+default:System.out.println("n0");
+
+}
+
+}
+}
+}
